2c0c9bca6bd172f9e0ca776ab7fc32085cd9e02e
afiq101 <afiq101@users.noreply.github.com>
2021-01-16 16:28:56 +0800

    overhaul design v2

---
+++ PMS/Controllers/LayoutController.cs
@@ -0,0 +1,23 @@
+﻿using PMS.Models;
+using PMS.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PMS.Controllers
+{
+    public class LayoutController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            photogEntities db = new photogEntities();
+            var userid = UserAuthentication.Identity().id;
+
+            var model = db.UserStudios.Where(x=>x.userid == userid).ToList();
+            return PartialView("~/Views/Shared/_LayoutStudioList.cshtml", model);
+        }
+    }
+}
